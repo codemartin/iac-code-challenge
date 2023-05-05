256720026667719ce9d677530d9196c2c8f62ce2
Martin Aponte <codemartin@gmail.com>
2023-05-05 16:13:20 -0500

    Update linty.sh

--- linty.sh
+++ linty.sh
@@ -4,9 +4,9 @@
 curl -L "$(curl -s https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64\.zip")" --output tflint.zip
 unzip tflint.zip
 rm tflint.zip
-#normally I would use sudo here but cnat be run on my machine via git bash 
+#normally I would use sudo here to move it but cnat be run on my machine via git bash 
 #sudo mv tflint /usr/local/bin/
-mv tflint /usr/local/bin/
+#mv tflint /usr/local/bin/
 
 # Run tflint on Terraform files
-tflint
+./tflint
